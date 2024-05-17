d50691c6a7edbef0b5b6b877c1b880c9eee55fbe
Ze <elawrence.bda@gmail.com>
2024-05-17 01:18:06 -0400

    Reworked search to be more basic and display one card accurately

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,65 +8,100 @@
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
 
+// const API
+const weatherForm = document.querySelector(".weatherForm");
+const cityInput = document.querySelector(".cityInput");
+const card = document.querySelector(".card");
+const apiKey = "baa5f2cd8b92122ca79c18d7b7213dc5";
 
+weatherForm.addEventListener("submit", async event => {
+    event.preventDefault();
+    const city = cityInput.value;
+    if(city){
+        try{
+            const weatherData = await getWeatherData(city);
+            displayWeatherInfo(weatherData);
+        }
+        catch(error){
+            console.error(error);
+            displayError(error);
+        }
+    }
+    else{
+        displayError("Please enter a city");
+    }
+});
 
-const userSearch = document.getElementsByClassName("weather-input");
-const searchHistoryEl = document.getElementsByClassName("history-div");
-const weatherDiv = document.getElementsByClassName("weather-div");
+async function getWeatherData(city){
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    const response = await fetch(apiUrl);
 
+    if(!response.ok){
+        throw new Error("Could not fetch weather data");
+    }
 
-const APIKey = "baa5f2cd8b92122ca79c18d7b7213dc5";
-
-
-// Checks current weather based on search
-function checkWeather(event) {
-    event.preventDefault()
-    const recentSearch = userSearch.value();
-
-    
+    return await response.json();
 }
 
+function displayWeatherInfo(data){
+    const {name: city, 
+           main: {temp, humidity}, 
+           weather: [{description, id}]} = data;
+    card.textContent = "";
+    card.style.display = "flex";
 
-// Gets current longitude and latitude to check current forecast
-function checkForecast(){
-    const apiURL = `https://api.openweathermap.org/geo/1.0/direct?q=${recentSearch}&limit=1&appid=${apiKey}`;
-    fetch(apiUrl)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            //console.log(data);
-            const userLong = JSON.parse(data[0].userLong);
-            const userLat = JSON.parse(data[0].userLat);
-            localStorage.setItem("lon", userLong);
-            localStorage.setItem("lat", userLat);
-            console.log(`Longitude: ${userLong} Latitude: ${userLat}`);
-        });
+    const cityDisplay = document.createElement("h1");
+    const tempDisplay = document.createElement("p");
+    const humidityDisplay = document.createElement("p");
+    const descDisplay = document.createElement("p");
+    const weatherEmoji = document.createElement("p");
+
+    cityDisplay.textContent = city;
+    tempDisplay.textContent = `${((temp - 273.15) * (9/5) + 32).toFixed(1)}°F`;
+    humidityDisplay.textContent = `Humidity: ${humidity}%`;
+    descDisplay.textContent = description;
+    weatherEmoji.textContent = getWeatherEmoji(id);
+    cityDisplay.classList.add("cityDisplay");
+    tempDisplay.classList.add("tempDisplay");
+    humidityDisplay.classList.add("humidityDisplay");
+    descDisplay.classList.add("descDisplay");
+    weatherEmoji.classList.add("weatherEmoji");
+
+    card.appendChild(cityDisplay);
+    card.appendChild(tempDisplay);
+    card.appendChild(humidityDisplay);
+    card.appendChild(descDisplay);
+    card.appendChild(weatherEmoji);
 }
 
-
-function displayWeatherResults() {
-    // Create a card to store all elements
-    // Create an area on the card to place elements
-    // Create card title
-    
-
-    // Add all elements to the card body
-
-    // Get required data
-    checkForecast();
-    checkWeather();
-
+function getWeatherEmoji(weatherId){
+    switch(true){
+        case (weatherId >= 200 && weatherId < 300):
+            return "⚡";
+        case (weatherId >= 300 && weatherId < 400):
+            return "🌧️";
+        case (weatherId >= 500 && weatherId < 600):
+            return "🌧️";
+        case (weatherId >= 600 && weatherId < 700):
+            return "❄️";
+        case (weatherId >= 700 && weatherId < 800):
+            return "";
+        case (weatherId === 800):
+            return "☀️";
+        case (weatherId >= 801 && weatherId < 810):
+            return "☁️";
+        default:
+            return "";
+    }
 }
 
-function showForecast(data){
+function displayError(message){
 
+    const errorDisplay = document.createElement("p");
+    errorDisplay.textContent = message;
+    errorDisplay.classList.add("errorDisplay");
+
+    card.textContent = "";
+    card.style.display = "flex";
+    card.appendChild(errorDisplay);
 }
-
-
-function setNewLocation(event){
-
-}
-
-searchButton.addEventListener("click", checkWeather);
-historyButton.addEventListener("click", setNewLocation);
